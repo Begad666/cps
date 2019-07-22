a2d8c6a4df7c648aac06fd36d12dd8c48efb2639
Begad666 <47504169+Begad666@users.noreply.github.com>
2019-07-22 14:02:04 +0300

    License

--- net/begad666/bc/plugin/customprotocolsettings/commands/CPS.java
+++ net/begad666/bc/plugin/customprotocolsettings/commands/CPS.java
@@ -36,7 +36,7 @@ public class CPS extends Command{
 			sender.sendMessage(new TextComponent(ProcessStrings.replacecodesandcolors(Config.getconfig().getString("prefix")) + " ---------------------------------"));
 			sender.sendMessage(new TextComponent(ProcessStrings.replacecodesandcolors(Config.getconfig().getString("prefix")) + " Version: " + Updates.getCurrentVersion()));
 			sender.sendMessage(new TextComponent(ProcessStrings.replacecodesandcolors(Config.getconfig().getString("prefix")) + " Updates: " + messagefromMessageinfo + " " + versionforupdating));
-			sender.sendMessage(new TextComponent(ProcessStrings.replacecodesandcolors(Config.getconfig().getString("prefix")) + " Minium Required version of jre: 1.8.1_191"));
+			sender.sendMessage(new TextComponent(ProcessStrings.replacecodesandcolors(Config.getconfig().getString("prefix")) + " Minimum Required version of jre: 1.8.1_191"));
 			sender.sendMessage(new TextComponent(ProcessStrings.replacecodesandcolors(Config.getconfig().getString("prefix")) + " #1417 version of BungeeCord is Minium Required"));
 			sender.sendMessage(new TextComponent(ProcessStrings.replacecodesandcolors(Config.getconfig().getString("prefix")) + " ---------------------------------"));
 		}
@@ -56,6 +56,10 @@ public class CPS extends Command{
 		renew(sender);	
 		else
 		sender.sendMessage(new TextComponent(ProcessStrings.replacecodesandcolors(Config.getconfig().getString("prefix")) + " No Renew is required"));
+		}
+		if ((args[0].equalsIgnoreCase("license")))
+		{
+		license(sender);
 		}
 
 	  }
@@ -86,5 +90,29 @@ public class CPS extends Command{
   {
 	  sender.sendMessage(new TextComponent(ProcessStrings.replacecodesandcolors(Config.getconfig().getString("prefix")) + " Checking what is needed to renew..."));
 	  Config.renew(sender, Config.getconfig().getString("config-version"));
+  }
+  public static void license(CommandSender sender)
+  {
+  sender.sendMessage(new TextComponent(ProcessStrings.replacecodesandcolors(Config.getconfig().getString("prefix")) + "\r\nMIT License\r\n" + 
+  		"\r\n" + 
+  		"Copyright (c) 2019 Begad666\r\n" + 
+  		"\r\n" + 
+  		"Permission is hereby granted, free of charge, to any person obtaining a copy\r\n" + 
+  		"of this software and associated documentation files (the \"Software\"), to deal\r\n" + 
+  		"in the Software without restriction, including without limitation the rights\r\n" + 
+  		"to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\r\n" + 
+  		"copies of the Software, and to permit persons to whom the Software is\r\n" + 
+  		"furnished to do so, subject to the following conditions:\r\n" + 
+  		"\r\n" + 
+  		"The above copyright notice and this permission notice shall be included in all\r\n" + 
+  		"copies or substantial portions of the Software.\r\n" + 
+  		"\r\n" + 
+  		"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\r\n" + 
+  		"IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\r\n" + 
+  		"FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\r\n" + 
+  		"AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\r\n" + 
+  		"LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\r\n" + 
+  		"OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\r\n" + 
+  		"SOFTWARE."));
   }
 }

920a4df4890742eaf54abaf0baefa4c480cd8e80
Begad666 <47504169+begad666@users.noreply.github.com>
2020-01-28 19:22:04 +0300

    Maven Test Directory
    Added maven test directory
    
    Signed-off-by: Begad666 <47504169+Begad666@users.noreply.github.com>

---
+++ src/test/java/test/jar/Main.java
@@ -0,0 +1,7 @@
+package test.jar;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World\nThis is a placeholder file for test\nArgs: " + args);
+    }
+}
